dea873afb8ff39a01ef4272741e898b9e655122d
zittoooo <stellar7484@naver.com>
2021-05-06 14:46:40 +0900

    ex01

---
+++ cpp01/ex01/ex01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+void memoryLeak()
+{
+    std::string *panther = new std::string("String panther");
+
+    std::cout << *panther << std::endl;
+    delete panther;
+}
+
+int main()
+{
+    memoryLeak();
+    system("leaks a.out");
+}
