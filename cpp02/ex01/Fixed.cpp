3cc616a2b80c45c7d5c61081f401469bc3494b4e
zittoooo <stellar7484@naver.com>
2021-05-11 22:29:30 +0900

    first

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,69 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    std::cout << "Default constructor called\n";
+    this->fixedPoint = 0;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called\n";
+}
+
+Fixed::Fixed(int const raw)
+{
+    std::cout << "Int constructor called\n";
+    this->fixedPoint = raw << this->fract;
+}
+
+Fixed::Fixed(float const raw)
+{
+    std::cout << "Float constructor called\n";
+    this->fixedPoint = roundf(raw * (1 << this->fract));
+}
+
+Fixed::Fixed(const Fixed& fixed)
+{
+    std::cout << "Copy constructor called\n";
+    // this->fixedPoint = fixed.getRawBits();
+    *this = fixed;
+}
+
+Fixed& Fixed::operator=(const Fixed& fixed)
+{
+    std::cout << "Assignation operator called\n";
+    this->fixedPoint = fixed.getRawBits();
+    return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+    // std::cout << "getRawBits member function called\n";
+    return (this->fixedPoint);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    // std::cout << "setRawBits member function called\n";
+    this->fixedPoint = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    float fpoint = (float)this->fixedPoint;
+    fpoint = fpoint / (1 << this->fract);
+    return (fpoint);
+}
+
+int Fixed::toInt(void) const
+{
+    int ipoint = this->fixedPoint >> this->fract;
+    return (ipoint);
+}
+
+std::ostream& operator<<(std::ostream& stm, const Fixed& fixed)
+{
+    stm << fixed.toFloat();
+    return (stm);
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+private:
+	int fixedPoint;
+	static const int fract = 8;
+public:
+	Fixed();
+	~Fixed();
+	Fixed(int const raw);
+	Fixed(float const raw);
+	Fixed(const Fixed& fixed);
+	Fixed&  operator=(const Fixed& fixed);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& stm, const Fixed& fixed);
+#endif
