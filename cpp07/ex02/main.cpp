a4505ed1e136abe7edb476cd5bfad96f3d4137a3
zittoooo <stellar7484@naver.com>
2021-06-03 13:31:04 +0900

    space

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -4,127 +4,132 @@
 
 void testEmpty()
 {
-  Array<int> empty;
-  try
-  {
-    empty[0] = 42;
-    std::cout << empty[0];
-  }
-  catch(const std::exception& e)
-  {
-    std::cout << e.what();
-  }
+    Array<int> empty;
+    try
+    {
+        empty[0] = 42;
+        std::cout << empty[0];
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what();
+    }
 }
 
 void testInt()
 {
-  try{
-    Array<int> arr(3);
-    for (int i = 0; i < 3; i++)
-      std::cout << arr[i] << ' ';
-    std::cout << '\n';
+    try
+    {
+        Array<int> arr(3);
+        for (int i = 0; i < 3; i++)
+            std::cout << arr[i] << ' ';
+        std::cout << '\n';
 
-    arr[0] = 1, arr[1] = 2, arr[2] = 3;
-    for (int i = 0; i < 3; i++)
-      std::cout << arr[i] << ' ';
-    std::cout << '\n';
-  }
-  catch(const std::exception& e)
-  {
-    std::cout << e.what();
-  }
+        arr[0] = 1, arr[1] = 2, arr[2] = 3;
+        for (int i = 0; i < 3; i++)
+            std::cout << arr[i] << ' ';
+        std::cout << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what();
+    }
 }
 
 void testString()
 {
-  try{
-    Array<std::string> sarr(3);
-    for (int i = 0; i < 3; i++)
-      std::cout << sarr[i] << ',';
-    std::cout << '\n';
+    try
+    {
+        Array<std::string> sarr(3);
+        for (int i = 0; i < 3; i++)
+            std::cout << sarr[i] << ',';
+        std::cout << '\n';
 
-    sarr[0] = "jiholee", sarr[1] = "42seoul", sarr[2] = "cpp07";
-    for (int i = 0; i < 3; i++)
-      std::cout << sarr[i] << ' ';
-    std::cout << '\n';
-  }
-  catch(const std::exception& e)
-  {
-    std::cout << e.what();
-  }
+        sarr[0] = "jiholee", sarr[1] = "42seoul", sarr[2] = "cpp07";
+        for (int i = 0; i < 3; i++)
+            std::cout << sarr[i] << ' ';
+        std::cout << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what();
+    }
 }
 
 void testAssignation()
 {
-  try{
-    Array<int> arr(3);
-    arr[0] = 1, arr[1] = 2, arr[2] = 3;
+    try
+    {
+        Array<int> arr(3);
+        arr[0] = 1, arr[1] = 2, arr[2] = 3;
 
-    Array<int> arr2;
-    arr2 = arr;
-    for (int i = 0; i < 3; i++)
-      std::cout << arr2[i] << ' ';
-    std::cout << '\n';
-  }
-  catch(const std::exception& e)
-  {
-    std::cout << e.what();
-  }
+        Array<int> arr2;
+        arr2 = arr;
+        for (int i = 0; i < 3; i++)
+            std::cout << arr2[i] << ' ';
+        std::cout << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what();
+    }
 }
 
 void testCopy()
 {
-  try{
-    Array<int> arr(3);
-    arr[0] = 1, arr[1] = 2, arr[2] = 3;
+    try
+    {
+        Array<int> arr(3);
+        arr[0] = 1, arr[1] = 2, arr[2] = 3;
 
-    Array<int> arr2(arr);
-    for (int i = 0; i < 3; i++)
-      std::cout << arr2[i] << ' ';
-    std::cout << '\n';
-  }
-  catch(const std::exception& e)
-  {
-    std::cout << e.what();
-  }
+        Array<int> arr2(arr);
+        for (int i = 0; i < 3; i++)
+            std::cout << arr2[i] << ' ';
+        std::cout << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what();
+    }
 }
 
 void testOutofRange()
 {
-  try{
-    Array<int> arr(3);
-    arr[0] = 1, arr[1] = 2, arr[2] = 3;
+    try
+    {
+        Array<int> arr(3);
+        arr[0] = 1, arr[1] = 2, arr[2] = 3;
 
-    Array<int> arr2(arr);
-    for (int i = 0; i < 10; i++)  // out of range
-      std::cout << arr2[i] << ' ';
-    std::cout << '\n';
-  }
-  catch(const std::exception& e)
-  {
-    std::cout << e.what();
-  }
+        Array<int> arr2(arr);
+        for (int i = 0; i < 10; i++) // out of range
+            std::cout << arr2[i] << ' ';
+        std::cout << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what();
+    }
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  if (argc != 2)
-    return 1;
+    if (argc != 2)
+        return 1;
 
     int testCase = argv[1][0] - '0';
     if (testCase == 0)
-      testEmpty();
+        testEmpty();
     if (testCase == 1)
-      testInt();
+        testInt();
     if (testCase == 2)
-      testString();
+        testString();
     if (testCase == 3)
-      testAssignation();
+        testAssignation();
     if (testCase == 4)
-      testCopy();
+        testCopy();
     if (testCase == 5)
-      testOutofRange();
+        testOutofRange();
 
-  std::cout << "\n\n";
-  system("leaks ex02");
+    std::cout << "\n\n";
+    system("leaks ex02");
 }
